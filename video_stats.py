3de2e421f0de341bf6b2dc1f04ea9046a1b83d36
Maria Karelioti <marka@netcompany.com>
2025-10-30 16:06:59 +0200

    save data

--- video_stats.py
+++ video_stats.py
@@ -3,6 +3,7 @@ import json
 
 import os
 from dotenv import load_dotenv
+from datetime import date
 
 load_dotenv(dotenv_path="./.env")
 
@@ -98,10 +99,15 @@ def extract_video_data(video_ids):
         return extracted_data
     except requests.exceptions.RequestException as e:
         raise e
-    
+
+def save_to_json(extracted_data):
+    file_path = f"./data/YT_data_{date.today()}.json"
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
+
 if __name__ == "__main__":
     playlist_id = get_playlist_id()
     video_ids = get_video_ids(playlist_id)
     video_data = extract_video_data(video_ids)
-    
+    save_to_json(video_data)
     
